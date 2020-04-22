f33c0bca9023c80e9cec804afb3377a7debbcc6b
AlphaGecko <mansour.jean-loup@protonmail.com>
2020-04-22 23:17:57 +0200

    save du 22/04/2020

--- OneBloc.js
+++ OneBloc.js
@@ -3,7 +3,7 @@ class OneBloc  {
     lien_image_1, url_partenaire_1, alt_1, description_site_1, description_projet_1,
     lien_image_2, url_partenaire_2, alt_2, description_site_2, description_projet_2,
     lien_image_3, url_partenaire_3, alt_3, description_site_3, description_projet_3, 
-    numberOfTheBloc
+    numberOfTheBloc,
     ) {
         this.lien_image_1 = lien_image_1;
         this.url_partenaire_1 = url_partenaire_1; 
@@ -32,7 +32,7 @@ class OneBloc  {
                 <div class="firstCase">
                     <a href=${this.url_partenaire_1}>
                         <p>
-                            <img src=${this.lien_image_1} alt=${this.alt_1}/>
+                            <img src=${this.lien_image_1} alt=${this.alt_1} onmouseenter="dynamicHoverPerBloc1(${this.numberOfTheBloc})" onmouseleave="dynamicHoverPerBloc1(${this.numberOfTheBloc})"/>
                             <span class="dynamic_background"></span>
                             <span class="description_image description_site_image_2">${this.description_site_1}</span>
                             <span class="description_image description_site_image_3">${this.description_projet_1}</span>
@@ -42,7 +42,7 @@ class OneBloc  {
                 <div class="secondCase">
                     <a href=${this.url_partenaire_2}>
                         <p>
-                            <img src=${this.lien_image_2} alt=${this.alt_2}/>
+                            <img src=${this.lien_image_2} alt=${this.alt_2} onmouseenter="dynamicHoverPerBloc2(${this.numberOfTheBloc})" onmouseleave="dynamicHoverPerBloc2(${this.numberOfTheBloc})"/>
                             <span class="dynamic_background"></span>
                             <span class="description_image description_site_image_1">${this.description_site_2}</span>
                             <span class="description_image description_projet_image_3">${this.description_projet_2}</span>
@@ -52,7 +52,7 @@ class OneBloc  {
                 <div class="thirdCase">
                     <a href=${this.url_partenaire_3}>
                         <p>
-                            <img src=${this.lien_image_3} alt=${this.alt_3}/>
+                            <img src=${this.lien_image_3} alt=${this.alt_3} onmouseenter="dynamicHoverPerBloc3(${this.numberOfTheBloc})" onmouseleave="dynamicHoverPerBloc3(${this.numberOfTheBloc})"/>
                             <span class="description_image dynamic_background"></span>
                             <span class="description_image description_projet_image_1">${this.description_site_3}</span>
                             <span class="description_image description_projet_image_2">${this.description_projet_3}</span>
--- custom.js
+++ custom.js
@@ -53,7 +53,99 @@ case6 = {
 let howManyBlocs = 0;
 let arrayStorage = [];
 
-blocCreation = (firstCase, secondCase, thirdCase) => {
+const dynamicHoverPerBloc1 = (blocNumber) => {
+
+    image1 = $('#number_' + blocNumber + ' .firstCase img');
+    descriptionSite1 = $('#number_' + blocNumber + ' .description_site_image_1');
+    descriptionProjet1 = $('#number_' + blocNumber + ' .description_projet_image_1');
+    image2 = $('#number_' + blocNumber + ' .secondCase img');
+    descriptionSite2 = $('#number_' + blocNumber + ' .description_site_image_2'); 
+    descriptionProjet2 = $('#number_' + blocNumber + ' .description_projet_image_2');
+    image3 = $('#number_' + blocNumber + ' .thirdCase img');
+    descriptionSite3 = $('#number_' + blocNumber + ' .description_site_image_3');
+    descriptionProjet3 = $('#number_' + blocNumber + ' description_projet_image_3');
+
+    image1.hover(
+        function () {
+            descriptionSite1.css({"opacity": "1", "z-index": "50"});
+            descriptionProjet1.css({"opacity": "1", "z-index": "50"});
+            descriptionSite2.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet2.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite3.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet3.css({"opacity": "0", "z-index": "-5"});
+        }, function () {
+            descriptionSite1.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet1.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite2.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet2.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite3.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet3.css({"opacity": "0", "z-index": "-5"});
+        }
+    )
+}
+
+const dynamicHoverPerBloc2 = (blocNumber) => {
+    image1 = $('#number_' + blocNumber + ' .firstCase img');
+    descriptionSite1 = $('#number_' + blocNumber + ' .description_site_image_1');
+    descriptionProjet1 = $('#number_' + blocNumber + ' .description_projet_image_1');
+    image2 = $('#number_' + blocNumber + ' .secondCase img');
+    descriptionSite2 = $('#number_' + blocNumber + ' .description_site_image_2'); 
+    descriptionProjet2 = $('#number_' + blocNumber + ' .description_projet_image_2');
+    image3 = $('#number_' + blocNumber + ' .thirdCase img');
+    descriptionSite3 = $('#number_' + blocNumber + ' .description_site_image_3');
+    descriptionProjet3 = $('#number_' + blocNumber + ' description_projet_image_3');
+
+    image2.hover(
+        function () {
+            descriptionSite1.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet1.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite2.css({"opacity": "1", "z-index": "50"});
+            descriptionProjet2.css({"opacity": "1", "z-index": "50"});
+            descriptionSite3.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet3.css({"opacity": "0", "z-index": "-5"});
+        }, function () {
+            descriptionSite1.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet1.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite2.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet2.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite3.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet3.css({"opacity": "0", "z-index": "-5"});
+        }
+    )
+}
+
+const dynamicHoverPerBloc3 = (blocNumber) => {
+    image1 = $('#number_' + blocNumber + ' .firstCase img');
+    descriptionSite1 = $('#number_' + blocNumber + ' .description_site_image_1');
+    descriptionProjet1 = $('#number_' + blocNumber + ' .description_projet_image_1');
+    image2 = $('#number_' + blocNumber + ' .secondCase img');
+    descriptionSite2 = $('#number_' + blocNumber + ' .description_site_image_2'); 
+    descriptionProjet2 = $('#number_' + blocNumber + ' .description_projet_image_2');
+    image3 = $('#number_' + blocNumber + ' .thirdCase img');
+    descriptionSite3 = $('#number_' + blocNumber + ' .description_site_image_3');
+    descriptionProjet3 = $('#number_' + blocNumber + ' description_projet_image_3');
+
+    image3.hover(
+        function () {
+            descriptionSite1.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet1.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite2.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet2.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite3.css({"opacity": "1", "z-index": "50"});
+            descriptionProjet3.css({"opacity": "1", "z-index": "50"});
+        }, function () {
+            descriptionSite1.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet1.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite2.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet2.css({"opacity": "0", "z-index": "-5"});
+            descriptionSite3.css({"opacity": "0", "z-index": "-5"});
+            descriptionProjet3.css({"opacity": "0", "z-index": "-5"});
+        }
+    )
+}
+
+const blocCreation = (firstCase, secondCase, thirdCase) => {
+
     let virtualBloc = new OneBloc(
         firstCase["lien_image"], firstCase["lien_partenaire"], firstCase["alt"], firstCase["description_site"], firstCase["description_projet"], 
         secondCase["lien_image"], secondCase["lien_partenaire"], secondCase["alt"], secondCase["description_site"], secondCase["description_projet"], 
