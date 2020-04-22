574f21d3d947fa613e97df21f217871463d74e35
AlphaGecko <mansour.jean-loup@protonmail.com>
2020-04-22 22:15:33 +0200

    Création d'une classe pour les effets de hover pour prévenir les bugs et faciliter l'utilisation du script

--- OneBloc.js
+++ OneBloc.js
@@ -3,6 +3,7 @@ class OneBloc  {
     lien_image_1, url_partenaire_1, alt_1, description_site_1, description_projet_1,
     lien_image_2, url_partenaire_2, alt_2, description_site_2, description_projet_2,
     lien_image_3, url_partenaire_3, alt_3, description_site_3, description_projet_3, 
+    numberOfTheBloc
     ) {
         this.lien_image_1 = lien_image_1;
         this.url_partenaire_1 = url_partenaire_1; 
@@ -21,11 +22,13 @@ class OneBloc  {
         this.alt_3 = alt_3;
         this.description_site_3 = description_site_3; 
         this.description_projet_3 = description_projet_3;
+
+        this.numberOfTheBloc = numberOfTheBloc;
     }
 
     addOneBlocToDOM = () => {
         return (
-            `<div class="oneBloc">
+            `<div class="oneBloc" id="number_${this.numberOfTheBloc}">
                 <div class="firstCase">
                     <a href=${this.url_partenaire_1}>
                         <p>
--- custom.js
+++ custom.js
@@ -3,51 +3,50 @@
 case1 = {
     lien_image: "./images/image1.jpg",
     lien_partenaire: "https://www.google.com",
-    alt: "partenaire numéro 1", 
-    description_site: "Ceci est ma description de site 1", 
-    description_projet: "Ceci est ma description de projet 1", 
+    alt: "partenaire_1", 
+    description_site: "Ceci est ma description de site de l'image 2", 
+    description_projet: "Ceci est ma description de site de l'image 3"
 }
 
 case2 = {
     lien_image: "./images/image2.jpg",
     lien_partenaire: "https://www.google.com",
-    alt: "partenaire numéro 2", 
-    description_site: "Ceci est ma description de site 2", 
-    description_projet: "Ceci est ma description de projet 2", 
+    alt: "partenaire_2", 
+    description_site: "Ceci est ma description de site de l'image 1", 
+    description_projet: "Ceci est ma description du projet de l'image 3"
 }
 
 case3 = {
     lien_image: "./images/image3.jpg",
     lien_partenaire: "https://www.google.com",
-    alt: "partenaire numéro 3", 
-    description_site: "Ceci est ma description de site 3", 
-    description_projet: "Ceci est ma description de projet 3"
+    alt: "partenaire_3", 
+    description_site: "Ceci est ma description du projet de l'image 1", 
+    description_projet: "Ceci est ma description du projet de l'image 2"
 }
 
 case4 = {
     lien_image: "./images/image1.jpg",
     lien_partenaire: "https://www.google.com",
-    alt: "partenaire numéro 4", 
-    description_site: "Ceci est ma description de site 4", 
-    description_projet: "Ceci est ma description de projet 4", 
+    alt: "partenaire_4", 
+    description_site: "Ceci est ma description de site de l'image 2", 
+    description_projet: "Ceci est ma description de site de l'image 3"
 }
 
 case5 = {
     lien_image: "./images/image2.jpg",
     lien_partenaire: "https://www.google.com",
-    alt: "partenaire numéro 5", 
-    description_site: "Ceci est ma description de site 5", 
-    description_projet: "Ceci est ma description de projet 5", 
+    alt: "partenaire_5", 
+    description_site: "Ceci est ma description de site de l'image 1", 
+    description_projet: "Ceci est ma description du projet de l'image 3" 
 }
 
 case6 = {
     lien_image: "./images/image3.jpg",
     lien_partenaire: "https://www.google.com",
-    alt: "partenaire numéro 6", 
-    description_site: "Ceci est ma description de site 6", 
-    description_projet: "Ceci est ma description de projet 6"
+    alt: "partenaire_6", 
+    description_site: "Ceci est ma description du projet de l'image 1", 
+    description_projet: "Ceci est ma description du projet de l'image 2"
 }
-
 
 /* Lancement du script selon les données que vous avez saisie. Enjoy ;) */
 
@@ -58,7 +57,8 @@ blocCreation = (firstCase, secondCase, thirdCase) => {
     let virtualBloc = new OneBloc(
         firstCase["lien_image"], firstCase["lien_partenaire"], firstCase["alt"], firstCase["description_site"], firstCase["description_projet"], 
         secondCase["lien_image"], secondCase["lien_partenaire"], secondCase["alt"], secondCase["description_site"], secondCase["description_projet"], 
-        thirdCase["lien_image"], thirdCase["lien_partenaire"], thirdCase["alt"], thirdCase["description_site"], thirdCase["description_projet"]
+        thirdCase["lien_image"], thirdCase["lien_partenaire"], thirdCase["alt"], thirdCase["description_site"], thirdCase["description_projet"], 
+        howManyBlocs
     );
     let blocContent = virtualBloc.addOneBlocToDOM();
     arrayStorage[howManyBlocs] = blocContent;
@@ -69,6 +69,5 @@ blocCreation(case1, case2, case3);
 blocCreation(case4, case5, case6);
 
 arrayStorage.forEach(bloc => {
-    $('#ref_container').prepend(bloc);
+    $('#ref_container').append(bloc);
 });
-
