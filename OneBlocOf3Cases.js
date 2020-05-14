e3093c2004c92e44ec3d016815cf64ff11b8a5f6
AlphaGecko <mansour.jean-loup@protonmail.com>
2020-05-14 15:41:05 +0200

    save avant modification responsive*

--- OneBlocOf3Cases.js
+++ OneBlocOf3Cases.js
@@ -1,26 +1,10 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) {
-  if (!(instance instanceof Constructor)) {
-    throw new TypeError("Cannot call a class as a function");
-  }
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function _defineProperties(target, props) {
-  for (var i = 0; i < props.length; i++) {
-    var descriptor = props[i];
-    descriptor.enumerable = descriptor.enumerable || false;
-    descriptor.configurable = true;
-    if ("value" in descriptor) descriptor.writable = true;
-    Object.defineProperty(target, descriptor.key, descriptor);
-  }
-}
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
-function _createClass(Constructor, protoProps, staticProps) {
-  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-  if (staticProps) _defineProperties(Constructor, staticProps);
-  return Constructor;
-}
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
 var OneBlocOf3Cases = /*#__PURE__*/function () {
   function OneBlocOf3Cases(lien_image_1, url_partenaire_1, alt_1, description_site_1, description_projet_1, lien_image_2, url_partenaire_2, alt_2, description_site_2, description_projet_2, lien_image_3, url_partenaire_3, alt_3, description_site_3, description_projet_3, numberOfTheBloc) {
@@ -49,14 +33,13 @@ var OneBlocOf3Cases = /*#__PURE__*/function () {
     value: function addOneBlocToDOM() {
       return "<div class=\"oneBloc\" id=\"number_".concat(this.numberOfTheBloc, "\">\n                <div class=\"firstCase\">\n                    <a href=").concat(this.url_partenaire_1, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                        <p>\n                            <img \n                                src=").concat(this.lien_image_1, " \n                                alt=").concat(this.alt_1, " \n                            \"/>\n                            <span class=\"dynamic_background\"></span>\n                            <span class=\"description_image description_site_image_2\">").concat(this.description_site_1, "</span>\n                            <span class=\"description_image description_site_image_3\">").concat(this.description_projet_1, "</span>\n                        </p>\n                    </a>\n                </div>\n                <div class=\"secondCase\">\n                    <a href=").concat(this.url_partenaire_2, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                        <p>\n                            <img \n                                src=").concat(this.lien_image_2, " \n                                alt=").concat(this.alt_2, " \n                            />\n                            <span class=\"dynamic_background\"></span>\n                            <span class=\"description_image description_site_image_1\">").concat(this.description_site_2, "</span>\n                            <span class=\"description_image description_projet_image_3\">").concat(this.description_projet_2, "</span>\n                        </p>\n                    </a>\n                </div>\n                <div class=\"thirdCase\">\n                    <a href=").concat(this.url_partenaire_3, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                        <p>\n                            <img \n                                src=").concat(this.lien_image_3, " \n                                alt=").concat(this.alt_3, " \n                            />\n                            <span class=\"dynamic_background\"></span>\n                            <span class=\"description_image description_projet_image_1\">").concat(this.description_site_3, "</span>\n                            <span class=\"description_image description_projet_image_2\">").concat(this.description_projet_3, "</span>\n                        </p>\n                    </a>\n                </div>\n            </div>");
     }
-  }]);
-
-  _createClass(OneBlocOf3Cases, [{
+  }, {
     key: "addOneDeviceBlocToDOM",
-    value: function addOneBlocToDOM() {
-      return "<div class=\"oneBloc_device\" id=\"number_".concat(this.numberOfTheBloc, "\">\n          <div class=\"firstCase_device\">\n              <a href=").concat(this.url_partenaire_1, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                  <p>\n                      <img src=").concat(this.lien_image_1, " alt=").concat(this.alt_1, "/>\n                  </p>\n              </a>\n              <p class=\"description_image_device description_site_image_1\">").concat(this.description_site_2, "</p>\n              <p class=\"description_image_device description_projet_image_1\">").concat(this.description_site_3, "</p>\n          </div>\n\n          <div class=\"secondCase_device\">\n              <a href=").concat(this.url_partenaire_2, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                <p>\n                    <img src=").concat(this.lien_image_2, " alt=").concat(this.alt_2, "\"/>\n                </p>\n              </a>\n              <p class=\"description_image_device description_site_image_2\">").concat(this.description_site_1, "</p>\n              <p class=\"description_image_device description_projet_image_2\">").concat(this.description_projet_3, "</p>\n          </div>\n\n          <div class=\"thirdCase_device\">\n              <a href=").concat(this.url_partenaire_3, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                <p>\n                    <img src=").concat(this.lien_image_3, " alt=").concat(this.alt_3, "/>\n                </p>\n              </a>\n              <p class=\"description_image_device description_site_image_3\">").concat(this.description_projet_1, "</p>\n              <p class=\"description_image_device description_projet_image_3\">").concat(this.description_projet_2, "</p>\n          </div>\n      </div>");
+    value: function addOneDeviceBlocToDOM() {
+      return "<div class=\"oneImage_device\">\n                <a href=".concat(this.url_partenaire_1, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                    <p>\n                      <img src=").concat(this.lien_image_1, " alt=").concat(this.alt_1, "/>\n                    </p>\n                </a>\n                <p class=\"description_image_device description_site_image_1\">").concat(this.description_site_2, "</p>\n                <p class=\"description_image_device description_projet_image_1\">").concat(this.description_site_3, "</p>\n              </div>\n\n              <div class=\"oneImage_device\">\n                <a href=").concat(this.url_partenaire_2, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                  <p>\n                    <img src=").concat(this.lien_image_2, " alt=").concat(this.alt_2, "/>\n                  </p>\n                </a>\n                <p class=\"description_image_device description_site_image_2\">").concat(this.description_site_1, "</p>\n                <p class=\"description_image_device description_projet_image_2\">").concat(this.description_projet_3, "</p>\n              </div>\n\n              <div class=\"oneImage_device\">\n                <a href=").concat(this.url_partenaire_3, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                  <p>\n                    <img src=").concat(this.lien_image_3, " alt=").concat(this.alt_3, "/>\n                  </p>\n                </a>\n                <p class=\"description_image_device description_site_image_3\">").concat(this.description_projet_1, "</p>\n                <p class=\"description_image_device description_projet_image_3\">").concat(this.description_projet_2, "</p>\n              </div>");
     }
   }]);
 
   return OneBlocOf3Cases;
 }();
+
--- script.js
+++ script.js
@@ -4,6 +4,69 @@
 /***** Lancement du script selon les données que vous avez saisies dans custom.js. Enjoy ;) *****/
 /************************************************************************************************/
 
+/* Detection dynamique des tailles et du media */ 
+
+var screenWidth = "";
+var windowWidth = "";
+var deviceDetection = "";
+var newScreenWidth = ""; 
+var newWindowWidth = ""; 
+var newDeviceDetection = ""; 
+
+
+/* Valeurs initiales de l'application */
+
+screenWidth = $(window).width();
+
+if (screenWidth < 1280) {
+    windowWidth = "small";
+} else {
+    windowWidth = "big";
+};
+
+if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || windowWidth === "small") {
+    deviceDetection = true;
+} else {
+    deviceDetection = false;
+};
+
+/* Detection dynamique des modifications de tailles d'écran */
+
+$(window).resize(function () {
+
+    newScreenWidth = $(window).width();
+
+    if (newScreenWidth < 1280) {
+        newWindowWidth = "small";
+    } else {
+        newWindowWidth = "big";
+    };
+
+    if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || newWindowWidth === "small") {
+        newDeviceDetection = true;
+    } else {
+        newDeviceDetection = false;
+    };
+
+    if (windowWidth !== newWindowWidth || deviceDetection !== newDeviceDetection) {
+        $('.oneBloc').remove();
+        $('.oneBloc_device').remove();
+
+        windowWidth = newWindowWidth;
+        deviceDetection = newDeviceDetection;
+        windowWidth = newWindowWidth;
+
+        var creationOfBlocs2 = new CreationOfBlocs();
+        creationOfBlocs2.AddDatasToArray();
+        creationOfBlocs2.arrayBlocStorage.forEach(function (bloc) {
+        $('.ref_container').append(bloc);
+        });
+        for (var i = 0; i < creationOfBlocs2.howManyBlocsHasBeenCreated; i++) {
+        creationOfBlocs2.AddStyleEventsToBlocs(i);
+        }
+    }
+});
+
 /* insère dynamiquement le nombre de blocs dans le tableau en fonction des entrées de l'utilisateur */
 
 var creationOfBlocs = new CreationOfBlocs();
@@ -20,60 +83,3 @@ creationOfBlocs.arrayBlocStorage.forEach(function (bloc) {
 for (var i = 0; i < creationOfBlocs.howManyBlocsHasBeenCreated; i++) {
   creationOfBlocs.AddStyleEventsToBlocs(i);
 }
-
-/* Valeurs initiales de l'application */
-
-var screenWidth;
-var windowWidth;
-var deviceDetection;
-screenWidth = $(window).width();
-
-if (screenWidth < 920) {
-  windowWidth = "small";
-} else {
-  windowWidth = "big";
-};
-
-if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || windowWidth === "small") {
-  deviceDetection = true;
-} else {
-  deviceDetection = false;
-};
-
-/* Detection dynamique des modifications de tailles d'écran */
-
-$(window).resize(function () {
-  var newWindowWidth;
-  var newScreenWidth;
-  var newDeviceDetection;
-  
-  newScreenWidth = $(window).width();
-
-  if (newScreenWidth < 920) {
-    newWindowWidth = "small";
-  } else {
-    newWindowWidth = "big";
-  };
-
-  if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || newWindowWidth === "small") {
-    newDeviceDetection = true;
-  } else {
-    newDeviceDetection = false;
-  };
-
-  if (windowWidth !== newWindowWidth || deviceDetection !== newDeviceDetection) {
-    $('.oneBloc').remove();
-    $('.oneBloc_device').remove();
-    var creationOfBlocs = new CreationOfBlocs();
-    creationOfBlocs.AddDatasToArray();
-    creationOfBlocs.arrayBlocStorage.forEach(function (bloc) {
-      $('.ref_container').append(bloc);
-    });
-    for (var i = 0; i < creationOfBlocs.howManyBlocsHasBeenCreated; i++) {
-      creationOfBlocs.AddStyleEventsToBlocs(i);
-    }
-    windowWidth = newWindowWidth;
-    deviceDetection = newDeviceDetection;
-    windowWidth = newWindowWidth;
-  }
-});
