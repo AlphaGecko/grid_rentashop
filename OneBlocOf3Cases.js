106a3e42a1d8acacdbdd26e1e0a8e3fe59921998
AlphaGecko <mansour.jean-loup@protonmail.com>
2020-06-05 18:13:32 +0200

    save du 05/06/2020

--- OneBlocOf3Cases.js
+++ OneBlocOf3Cases.js
@@ -1,10 +1,26 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
 
 var OneBlocOf3Cases = /*#__PURE__*/function () {
   function OneBlocOf3Cases(lien_image_1, url_partenaire_1, alt_1, description_site_1, description_projet_1, lien_image_2, url_partenaire_2, alt_2, description_site_2, description_projet_2, lien_image_3, url_partenaire_3, alt_3, description_site_3, description_projet_3, numberOfTheBloc) {
@@ -36,52 +52,9 @@ var OneBlocOf3Cases = /*#__PURE__*/function () {
   }, {
     key: "addOneDeviceBlocToDOM",
     value: function addOneDeviceBlocToDOM() {
-      return `
-        <div class="one_bloc_device">
-          <div class="draggable_container">
-            <div class="draggable_content">
-              <div class="oneImage_device">
-                <a href=${this.url_partenaire_1} target="_blank" rel="noopener noreferrer nofollow">
-                    <p>
-                      <img src=${this.lien_image_1} alt=${this.alt_1}/>
-                    </p>
-                </a>
-              </div>
-              <div class="oneText_device">
-                <p class="description_image_device description_site_image_1">${this.description_site_2}</p>
-                <p class="description_image_device description_projet_image_1">${this.description_site_3}</p>
-              </div>
-
-              <div class="oneImage_device">
-                <a href=${this.url_partenaire_2} target="_blank" rel="noopener noreferrer nofollow">
-                  <p>
-                    <img src=${this.lien_image_2} alt=${this.alt_2}/>
-                  </p>
-                </a>
-              </div>
-              <div class="oneText_device">
-                <p class="description_image_device description_site_image_2">${this.description_site_1}</p>
-                <p class="description_image_device description_projet_image_2">${this.description_projet_3}</p>
-              </div>
-
-              <div class="oneImage_device">
-                <a href=${this.url_partenaire_3} target="_blank" rel="noopener noreferrer nofollow">
-                  <p>
-                    <img src=${this.lien_image_3} alt=${this.alt_3}/>
-                  </p>
-                </a>
-              </div>
-              <div class="oneText_device">
-                <p class="description_image_device description_site_image_3">${this.description_projet_1}</p>
-                <p class="description_image_device description_projet_image_3">${this.description_projet_2}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        `
-      }
+      return "\n        <div class=\"one_bloc_device\">\n          <div class=\"draggable_container\">\n            <div class=\"draggable_content\">\n              <div class=\"oneImage_device\">\n                <a href=".concat(this.url_partenaire_1, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                    <p>\n                      <img src=").concat(this.lien_image_1, " alt=").concat(this.alt_1, "/>\n                    </p>\n                </a>\n              </div>\n              <div class=\"oneText_device\">\n                <p class=\"description_image_device description_site_image_1\">").concat(this.description_site_2, "</p>\n                <p class=\"description_image_device description_projet_image_1\">").concat(this.description_site_3, "</p>\n              </div>\n\n              <div class=\"oneImage_device\">\n                <a href=").concat(this.url_partenaire_2, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                  <p>\n                    <img src=").concat(this.lien_image_2, " alt=").concat(this.alt_2, "/>\n                  </p>\n                </a>\n              </div>\n              <div class=\"oneText_device\">\n                <p class=\"description_image_device description_site_image_2\">").concat(this.description_site_1, "</p>\n                <p class=\"description_image_device description_projet_image_2\">").concat(this.description_projet_3, "</p>\n              </div>\n\n              <div class=\"oneImage_device\">\n                <a href=").concat(this.url_partenaire_3, " target=\"_blank\" rel=\"noopener noreferrer nofollow\">\n                  <p>\n                    <img src=").concat(this.lien_image_3, " alt=").concat(this.alt_3, "/>\n                  </p>\n                </a>\n              </div>\n              <div class=\"oneText_device\">\n                <p class=\"description_image_device description_site_image_3\">").concat(this.description_projet_1, "</p>\n                <p class=\"description_image_device description_projet_image_3\">").concat(this.description_projet_2, "</p>\n              </div>\n            </div>\n          </div>\n        </div>\n        ");
+    }
   }]);
 
   return OneBlocOf3Cases;
 }();
-
--- script.js
+++ script.js
@@ -1,113 +1,120 @@
 "use strict";
-
 /************************************************************************************************/
+
 /***** Lancement du script selon les données que vous avez saisies dans custom.js. Enjoy ;) *****/
+
 /************************************************************************************************/
 
 var screenWidth = "";
 var windowWidth = "";
 var deviceDetection = "";
-
 /* Valeurs initiales de l'application */
 
 screenWidth = $(window).width();
 
 if (screenWidth < 1280) {
-    windowWidth = "small";
+  windowWidth = "small";
 } else {
-    windowWidth = "big";
-};
+  windowWidth = "big";
+}
 
+;
 
 if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || windowWidth === "small") {
-    deviceDetection = true;
+  deviceDetection = true;
 } else {
-    deviceDetection = false;
-};
+  deviceDetection = false;
+}
 
+;
 /* insère dynamiquement le nombre de blocs dans le tableau en fonction des entrées de l'utilisateur */
 
 var creationOfBlocs = new CreationOfBlocs();
 creationOfBlocs.AddDatasToArray();
-
 /* intégration des blocs HTML du tableau au DOM selon si l'utilisateur utiliseur IE ou un autre navigateur */
 
 creationOfBlocs.arrayBlocStorage.forEach(function (bloc) {
   $('.ref_container').append(bloc);
 });
-
 /* Ajout des styles dynamiques au DOM */
 
 for (var i = 0; i < creationOfBlocs.howManyBlocsHasBeenCreated; i++) {
   creationOfBlocs.AddStyleEventsToBlocs(i);
 }
+/* debug des ancres sur IE */
 
-/* debug des ancres sur IE */ 
 
 var ua = window.navigator.userAgent;
 var msie = ua.indexOf('MSIE ');
 var trident = ua.indexOf('Trident/');
 
 if (msie > 0 || trident > 0) {
-    console.log('IE detecté, remise en forme du document');
-    var imageWidth = $("body > div > div:nth-child(1) > a > p > img").css("width");
-    $(".oneImage_device > a").css("height", imageWidth);
+  console.log('IE detecté, remise en forme du document');
+  var imageWidth = $("body > div > div:nth-child(1) > a > p > img").css("width");
+  $(".oneImage_device > a").css("height", imageWidth);
 }
+/* rend le contenu draggable sur téléphone */
 
-/* rend le contenu draggable sur téléphone */ 
 
-$(".draggable_content").draggable({axis: "x", containment: ".draggable_container", scroll: false});
-
+$(".draggable_content").draggable({
+  axis: "x",
+  containment: ".draggable_container",
+  scroll: false
+});
 /* Detection dynamique des modifications de tailles d'écran */
 
 $(window).resize(function () {
+  /* debug des ancres sur IE */
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf('MSIE ');
+  var trident = ua.indexOf('Trident/');
 
-    /* debug des ancres sur IE */ 
+  if (msie > 0 || trident > 0) {
+    console.log('IE detecté, remise en forme du document');
+    var imageWidth = $("body > div > div:nth-child(1) > a > p > img").css("width");
+    $(".oneImage_device > a").css("height", imageWidth);
+  }
 
-    var ua = window.navigator.userAgent;
-    var msie = ua.indexOf('MSIE ');
-    var trident = ua.indexOf('Trident/');
+  var newScreenWidth = "";
+  var newWindowWidth = "";
+  newScreenWidth = $(window).width();
 
-    if (msie > 0 || trident > 0) {
-        console.log('IE detecté, remise en forme du document');
-        var imageWidth = $("body > div > div:nth-child(1) > a > p > img").css("width");
-        $(".oneImage_device > a").css("height", imageWidth);
+  if (newScreenWidth < 1280) {
+    newWindowWidth = "small";
+  } else {
+    newWindowWidth = "big";
+  }
+
+  ;
+
+  if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || newWindowWidth === "small") {
+    deviceDetection = true;
+  } else {
+    deviceDetection = false;
+  }
+
+  ;
+
+  if (windowWidth !== newWindowWidth) {
+    $('.oneBloc').remove();
+    $('.one_bloc_device').remove();
+    var creationOfBlocs = new CreationOfBlocs();
+    creationOfBlocs.AddDatasToArray();
+    creationOfBlocs.arrayBlocStorage.forEach(function (bloc) {
+      $('.ref_container').append(bloc);
+    });
+
+    for (var i = 0; i < creationOfBlocs.howManyBlocsHasBeenCreated; i++) {
+      creationOfBlocs.AddStyleEventsToBlocs(i);
     }
 
-    var newScreenWidth = ""; 
-    var newWindowWidth = ""; 
-
-    newScreenWidth = $(window).width();
-
-    if (newScreenWidth < 1280) {
-        newWindowWidth = "small";
-    } else {
-        newWindowWidth = "big";
-    };
-
-    if (navigator.userAgent.match(/(Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini)/gi) || newWindowWidth === "small") {
-        deviceDetection = true;
-    } else {
-        deviceDetection = false;
-    };
-
-    if (windowWidth !== newWindowWidth) {
-
-        $('.oneBloc').remove();
-        $('.one_bloc_device').remove();
-        var creationOfBlocs = new CreationOfBlocs();
-        creationOfBlocs.AddDatasToArray();
-        creationOfBlocs.arrayBlocStorage.forEach(function (bloc) {
-        $('.ref_container').append(bloc);
-        });
-        for (var i = 0; i < creationOfBlocs.howManyBlocsHasBeenCreated; i++) {
-        creationOfBlocs.AddStyleEventsToBlocs(i);
-        }
-
-        windowWidth = newWindowWidth;
-        screenWidth = newScreenWidth;
-
-        $(".draggable_content").disableSelection();
-        $(".draggable_content").draggable({axis: "x", containment: ".draggable_container", scroll: false});
-    } 
+    windowWidth = newWindowWidth;
+    screenWidth = newScreenWidth;
+    $(".draggable_content").disableSelection();
+    $(".draggable_content").draggable({
+      axis: "x",
+      containment: ".draggable_container",
+      scroll: false
+    });
+  }
 });
